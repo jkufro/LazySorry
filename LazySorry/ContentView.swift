787fe8457509ea74bfb64bed29c8bf3c13571cd0
Justin Kufro <kufroj@gmail.com>
2020-07-09 18:00:32 -0400

    Initial Commit

---
+++ LazySorry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LazySorry
+//
+//  Created by Justin Kufro on 7/9/20.
+//  Copyright © 2020 Justin Kufro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
